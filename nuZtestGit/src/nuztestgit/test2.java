68e5eae712e025ed47d795907eedbe912e329b72
Amin-PC\Amin <nusran7@gmail.com>
2018-09-23 01:08:34 +0530

    upload file from ntbns

---
+++ nuZtestGit/src/nuztestgit/test2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuztestgit;
+
+/**
+ *
+ * @author Amin
+ */
+public class test2 {
+    //this is create new class from ntbeans test
+}
